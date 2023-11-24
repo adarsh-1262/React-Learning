482917a20935c38151b541daab9df12b9cd26ab6
ADARSH SINGH <110599099+adarsh-1262@users.noreply.github.com>
2023-11-24 13:39:23 +0530

    Cleared component making

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+
+    <h1>hello everyone</h1>
+    <Chai />
+    
+    </>
+   
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./Chai"
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>good morninggg....</h1>
+    <Chai/>
+    </>
+   
+  )
+}
+
+export default App
