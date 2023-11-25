7a7340879f552941269358f07c4b396fb5ee990c
ADARSH SINGH <110599099+adarsh-1262@users.noreply.github.com>
2023-11-25 10:34:55 +0530

    creating own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,13 @@ import Chai from "./Chai"
 
 
 function App() {
-
+  const username = "Adarsh Singh"
 
   return (
     <>
-    <h1>good morninggg....</h1>
+    <h1>good morninggg from {username}....</h1>
     <Chai/>
+    <p>testing paragraph......</p>
     </>
    
   )
